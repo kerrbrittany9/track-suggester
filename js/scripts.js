5e63761e783febf08d5043efee5f18d82b141777
BrittanyKerr <kerrbrittany9@gmail.com>
2017-06-02 12:46:00 -0700

    add class form-group to survey to create space between questions and enlarge margins in well

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,29 @@ $(document).ready(function() {
     var tech_interest = $("select#tech_interest").val();
     var app = $("select#app").val();
     var indicator = $("select#indicator").val();
-//
-// // console.log('test')
-//     if (interests === 'math' && tech_interest === 'frontend') {
-//       $('#CSS').show();
-//       alert("Hey! How about learning CSS?");
-//     } else if (work_environment === 'big' || indicator === 'windows') {
-//       $('#Csharp').show();
-//       alert('Get ready for a sweet career in C#!');
-      else  {
-//       $('#Ruby').show();
-//       alert('Don't blush too much about learning Ruby!');
-//     }
-//       $("#Ruby").click(function() {
-//       $(this).remove();
-//     });
+
+    if (tech_interest === 'frontend' ||interests === 'math') {
+      $('#CSS').show();
+      alert("Hey! How about learning CSS?");
+    // }
+    // else if (work_environment === 'big' || indicator ===  'windows') {
+    //   $('#Csharp').show();
+    //   alert('Get ready for a sweet career in C#!');
+    // }
+    // else {
+    //   $('#Ruby').show();
+    //   alert("Don't blush too much about learning Ruby!");
+    // }
+    //     $("#CSS").click(function() {
+    //     $(this).remove();
+    //     $("#Csharp").click(function() {
+    //     $(this).remove();
+    //     $("#Ruby").click(function() {
+    //     $(this).remove();
+    //
+    // console.log('test')
+      });
+    });
   });
+});
 });
